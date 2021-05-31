410a24508de550ba423e344841dfc8b88466b4b7
unknown <sumit29srivastava@gmail.com>
2021-05-31 15:14:55 +0530

    fide

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+    type = string
+    description = ""
+}
+
+variable "saccesskey" {
+    type = string
+}
+
+variable "secretname" {
+    type = string
+}
+
+variables "proxyname"{
+    type = string
+}
